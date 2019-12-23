73c80ddde5a27c4cd65840687be9cecc37db62eb
vasilio13 <58145288+vasilio13@users.noreply.github.com>
2019-12-23 21:41:58 +0300

    Add files via upload
    homework

---
+++ Dump20191223.sql
@@ -0,0 +1,143 @@
+CREATE DATABASE  IF NOT EXISTS `testdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `testdb`;
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: testdb
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `laptop`
+--
+
+DROP TABLE IF EXISTS `laptop`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `laptop` (
+  `code` int(11) NOT NULL,
+  `model` varchar(50) DEFAULT NULL,
+  `speed` smallint(6) DEFAULT NULL,
+  `ram` smallint(6) DEFAULT NULL,
+  `hd` double DEFAULT NULL,
+  `price` decimal(13,2) DEFAULT NULL,
+  `screen` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`code`),
+  KEY `fk_model_product_idx` (`model`),
+  CONSTRAINT `fk_laptop_model_product_model` FOREIGN KEY (`model`) REFERENCES `product` (`model`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `laptop`
+--
+
+LOCK TABLES `laptop` WRITE;
+/*!40000 ALTER TABLE `laptop` DISABLE KEYS */;
+/*!40000 ALTER TABLE `laptop` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pc`
+--
+
+DROP TABLE IF EXISTS `pc`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pc` (
+  `code` int(11) NOT NULL,
+  `model` varchar(50) DEFAULT NULL,
+  `speed` smallint(6) DEFAULT NULL,
+  `ram` smallint(6) DEFAULT NULL,
+  `hd` double DEFAULT NULL,
+  `cd` varchar(10) DEFAULT NULL,
+  `price` decimal(13,2) DEFAULT NULL,
+  PRIMARY KEY (`code`),
+  KEY `fk_model_product_idx` (`model`),
+  CONSTRAINT `fk_model_product` FOREIGN KEY (`model`) REFERENCES `product` (`model`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pc`
+--
+
+LOCK TABLES `pc` WRITE;
+/*!40000 ALTER TABLE `pc` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pc` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `printer`
+--
+
+DROP TABLE IF EXISTS `printer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `printer` (
+  `code` int(11) NOT NULL,
+  `model` varchar(45) DEFAULT NULL,
+  `colour` char(1) DEFAULT NULL,
+  `type` varchar(10) DEFAULT NULL,
+  `price` decimal(13,2) DEFAULT NULL,
+  `printercol` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`code`),
+  KEY `fk_printer_model_product_model_idx` (`model`),
+  CONSTRAINT `fk_printer_model_product_model` FOREIGN KEY (`model`) REFERENCES `product` (`model`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `printer`
+--
+
+LOCK TABLES `printer` WRITE;
+/*!40000 ALTER TABLE `printer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `printer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `maker` varchar(10) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  PRIMARY KEY (`model`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES ('apple','ap777','laptop'),('f4','d3300','pc'),('f2','d4400','pc'),('f1','f1003','pc'),('acer','lt200l','laptop'),('lenovo','lt205l','laptop'),('canon','pr100c','printer'),('hp','pr101h','printer');
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-23 21:37:34
